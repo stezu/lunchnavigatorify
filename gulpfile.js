c7f50cbcd717ceed6ba49f2390c6ca5e4e9d414d
jvalle <jcvalle@mail.usf.edu>
2014-06-06 14:46:05 -0400

    i am moving forward with this gulp config, for now

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,17 @@ gulp.task('lint', function () {
     var jshint = require('gulp-jshint'),
         stylish = require('jshint-stylish');
 
-    gulp.src('./public/js/*.js')
+    gulp.src(['./public/js/*.js', './routes/*.js', './app.js'])
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('uglify', function () {
+    var uglify = require('gulp-uglify');
+
+    gulp.src('./public/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/js'));
 });
 
 gulp.task('stylus', function () {
