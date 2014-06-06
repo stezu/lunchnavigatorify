376a55c8a3fab4d1431c2722ec8a171f870187cf
jvalle <jcvalle@mail.usf.edu>
2014-06-06 12:24:42 -0400

    configure jshint complete with dumb test

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp');
-var stylus = require('gulp-stylus');
+
+gulp.task('lint', function () {
+    var jshint = require('gulp-jshint'),
+        stylish = require('jshint-stylish');
+
+    gulp.src('./public/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
 
 gulp.task('stylus', function () {
+    var stylus = require('gulp-stylus');
+
     gulp.src('./public/styl/**/*.styl')
         .pipe(stylus())
         .pipe(gulp.dest('./public/styles'));
 });
 
-gulp.task('default', ['stylus']);
+gulp.task('default', ['stylus', 'lint']);
