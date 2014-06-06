a508c1555ac2e94f9a93ecd07180e6a69201b239
jvalle <jcvalle@mail.usf.edu>
2014-06-06 16:22:17 -0400

    hook up form field to submit to database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../modules/database');
+
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    res.render('index', { title: 'lunchnavigatorify' });
+});
+
+router.post('/', function (req, res) {
+    db.saveLocation({
+        name: req.param('lunchspot')
+    }, function (err, saved) {
+        if (err) {
+            console.log('There was an error saving the article, i think?' + err);
+        } else {
+            console.log('nice job.');
+        }
+    });
 });
 
 module.exports = router;
