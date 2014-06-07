1537171901d9fad6b6f056eb9c9333d794cab24b
Jason <jcvalle@mail.usf.edu>
2014-06-06 22:05:31 -0500

    add a yelptest, route, view and result view
    to test, go to /yelp-search and search that shit

--- modules/yelp.js
+++ modules/yelp.js
@@ -1,7 +1,6 @@
-var config = require('../appconfig'),
-    yelp = require('yelp');
+var config = require('../appconfig');
 
-yelp.createClient({
+var yelp = require('yelp').createClient({
     consumer_key: config.yelp.consumer_key,
     consumer_secret: config.yelp.consumer_secret,
     token: config.yelp.token,
@@ -12,9 +11,10 @@ exports.search = function (query, callback) {
     // we can totally make this super robust so you can pass an object with any number of options and
     // we can loop through the names and match them to their values
     // this will make for a nice interface with tons of options
+    console.log('we get in searchFor');
     yelp.search({
         term: query.term,
-        location: query.locationDetails
+        location: query.location
     }, function (err, data) {
         if (err) {
             console.log('There was a problem in yelp.search and it was ' + err);
