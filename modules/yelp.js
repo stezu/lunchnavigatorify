fca5981711da4e025a0ded1f06edf242ea96f3d5
Jason <jcvalle@mail.usf.edu>
2014-06-06 20:12:32 -0500

    a yelp search function is now also in the house

--- modules/yelp.js
+++ modules/yelp.js
@@ -1,3 +1,24 @@
 var config = require('../appconfig'),
     yelp = require('yelp');
 
+yelp.createClient({
+    consumer_key: config.yelp.consumer_key,
+    consumer_secret: config.yelp.consumer_secret,
+    token: config.yelp.token,
+    token_secret: config.yelp.token_secret
+});
+
+exports.search = function (query, callback) {
+    // we can totally make this super robust so you can pass an object with any number of options and
+    // we can loop through the names and match them to their values
+    // this will make for a nice interface with tons of options
+    yelp.search({
+        term: query.term,
+        location: query.locationDetails
+    }, function (err, data) {
+        if (err) {
+            console.log('There was a problem in yelp.search and it was ' + err);
+        }
+        callback(err, data);
+    });
+};
