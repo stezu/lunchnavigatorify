5a99a36850a3a04011211dc6f0dcc76aa3e610c2
Jason <jcvalle@mail.usf.edu>
2014-06-09 00:20:07 -0500

    add a findOrg db method to lookup organization based on slug
    to test, check out /org/health-is and /org/fart-town.
    really a proof of concept.
    results.jade view needs to be updated to allow passing of restaurant
    data stored in organization table, too.  if we even need it to display
    yelp data.

--- modules/database.js
+++ modules/database.js
@@ -1,14 +1,14 @@
 var config = require('../appconfig.js'),
     mongojs = require('mongojs');
 
-var db = mongojs(config.mongo.mongo_uri, ['restaurants']);
+var db = mongojs(config.mongo.mongo_uri, ['restaurants', 'organizations']);
 
 exports.getObjectId = function (id) {
     return mongojs.ObjectId(id);
 };
 
-exports.findAll = function (callback) {
-    db.restaurants.find().toArray(function (err, results) {
+exports.findOrganization = function (query, callback) {
+    db.organizations.find(query, function (err, results) {
         if (err) {
             console.log('There was an error in db.findAll ' + err);
         }
--- routes/organization.js
+++ routes/organization.js
@@ -4,7 +4,18 @@ var router = express.Router();
 var db = require('../modules/database');
 
 router.get('/:organization', function (req, res) {
-    res.send(req.param('organization'));
+    db.findOrganization({ slug: req.param('organization') }, function (err, results) {
+        if (err) {
+            // this should send a friendly mesaage to the user that maybe triggers an alert or something
+            console.log('There was an error retriving your organization\'s restaurants.');
+        } else {
+            console.log(results[0].restaurants);
+            res.render('index', {
+                title: results[0].name + ' lunch spots',
+                locations: results[0].restaurants
+            });
+        }
+    });
 });
 
 module.exports = router;
