5ea54b1e8d0721b3678f5e0ac6572138c4c0ea5a
Stephen Zuniga <stephen.zuniga001@gmail.com>
2014-12-17 17:49:20 -0500

    Add default app config
    so people can know the format

---
+++ appconfig.default.js
@@ -0,0 +1,13 @@
+var appConfig = {
+    'yelp': {
+        'consumer_key': '',
+        'consumer_secret': '',
+        'token': '',
+        'token_secret': ''
+    },
+    'mongo': {
+        'mongo_uri': ''
+    }
+};
+
+module.exports = appConfig;
