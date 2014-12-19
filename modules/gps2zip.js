2e1f459eb2bda4d9181a283acc36864099dfe0d1
Jason <jcvalle@mail.usf.edu>
2014-12-18 22:44:35 -0700

    module to utilize gps2zip

--- modules/gps2zip.js
+++ modules/gps2zip.js
@@ -1,7 +1,7 @@
 var gps = require('gps2zip');
 
-module.exports = function (position) {
-    console.log(gps.gps2zip(position.coords.latitude, position.coords.longitude));
-    
-    return gps.gps2zip(position.coords.latitude, position.coords.longitude);
+module.exports = function (lat, lon) {
+    console.log(gps.gps2zip(lat, lon));
+
+    return gps.gps2zip(lat, lon);
 };
