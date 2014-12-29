7721bc24a897e60ce546dba15221cf0a37d95b37
Stephen Zuniga <stephen.zuniga001@gmail.com>
2014-12-29 16:28:32 -0500

    make all the modules the same

--- modules/auth.js
+++ modules/auth.js
@@ -1,34 +1,42 @@
-var config = require('../appconfig.js');
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var auth = (function () {
+    "use strict";
 
-exports.init = function () {
-    passport.use(new GoogleStrategy({
-            clientID: config.google.clientID,
-            clientSecret: config.google.clientSecret,
-            callbackURL: config.appUrl + 'login/google/callback',
+    var config = require('../appconfig.js'),
+        passport = require('passport'),
+        GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+    return {
+        init: function () {
+            passport.use(new GoogleStrategy({
+                    clientID: config.google.clientID,
+                    clientSecret: config.google.clientSecret,
+                    callbackURL: config.appUrl + 'login/google/callback',
+                },
+                function(accessToken, refreshToken, profile, done) {
+                    // console.log(accessToken, refreshToken, profile, done);
+
+                    return done(null, profile);
+                }
+            ));
+
+            passport.serializeUser(function(user, done) {
+                done(null, user);
+            });
+
+            passport.deserializeUser(function(user, done) {
+                done(null, user);
+            });
+
+            return passport;
         },
-        function(accessToken, refreshToken, profile, done) {
-            // console.log(accessToken, refreshToken, profile, done);
 
-            return done(null, profile);
+        ensure: function (req, res, next) {
+            if (req.isAuthenticated()) { return next(); }
+            req.session.returnTo = req.originalUrl;
+
+            res.redirect('/login/google');
         }
-    ));
+    };
+}());
 
-    passport.serializeUser(function(user, done) {
-        done(null, user);
-    });
-
-    passport.deserializeUser(function(user, done) {
-        done(null, user);
-    });
-
-    return passport;
-};
-
-exports.ensure = function (req, res, next) {
-    if (req.isAuthenticated()) { return next(); }
-    req.session.returnTo = req.originalUrl;
-
-    res.redirect('/login/google');
-};
+module.exports = auth;
--- modules/yelp.js
+++ modules/yelp.js
@@ -1,17 +1,25 @@
-var config = require('../appconfig');
+var yelp = (function () {
+    "use strict";
 
-var yelp = require('yelp').createClient({
-    consumer_key: config.yelp.consumer_key,
-    consumer_secret: config.yelp.consumer_secret,
-    token: config.yelp.token,
-    token_secret: config.yelp.token_secret
-});
+    var config = require('../appconfig');
 
-exports.search = function (query, callback) {
-    yelp.search(query, function (err, data) {
-        if (err) {
-            console.log('There was a problem in yelp.search and it was', err);
-        }
-        callback(err, data);
+    var yelp = require('yelp').createClient({
+        consumer_key: config.yelp.consumer_key,
+        consumer_secret: config.yelp.consumer_secret,
+        token: config.yelp.token,
+        token_secret: config.yelp.token_secret
     });
-};
+
+    return {
+        search: function (query, callback) {
+            yelp.search(query, function (err, data) {
+                if (err) {
+                    console.log('There was a problem in yelp.search and it was', err);
+                }
+                callback(err, data);
+            });
+        }
+    };
+}());
+
+module.exports = yelp;
