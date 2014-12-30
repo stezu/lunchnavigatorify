9895bf427f60e2e26315ec55c1e4c416622701f4
Stephen Zuniga <stephen.zuniga001@gmail.com>
2014-12-30 13:49:58 -0500

    reformat orgs to reduce db requests

--- routes/organization.js
+++ routes/organization.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+var config = require('../appconfig.js');
 var db = require('../modules/database');
 var auth = require('../modules/auth');
 
@@ -52,66 +53,80 @@ router.use('/:organization', function (req, res, next) {
 });
 
 router.get('/:organization', function (req, res) {
-    db.findOne('organizations', { slug: req.param('organization') }, function (err, results) {
+    db.findOne('organizations', { 'slug': req.param('organization') }, function (err, results) {
         if (err) {
-            // this should send a friendly mesaage to the user that maybe triggers an alert or something
+            // this should send a friendly message to the user that maybe triggers an alert or something
             console.log('There was an error retrieving your organization\'s restaurants.');
         } else {
-            // console.log(results);
             res.render('index', {
-                title: results.name + ' lunch spots',
-                zip: results.zip,
-                locations: results.restaurants ? results.restaurants : []
+                'title'    : results.name + ' lunch spots',
+                'zip'      : results.zip,
+                'locations': results.restaurants ? results.restaurants : []
             });
         }
     });
 });
 
 router.post('/:organization', function (req, res) {
-    var restaurant = {
-            _id: db.makeObjectId(),
-            yelp: {
-                name:      req.body.name ? req.body.name : null,
-                id:        req.body.id ? req.body.id : null,
-                location:  req.body.location ? req.body.location : null,
-                is_closed: req.body.is_closed ? req.body.is_closed : null,
-                url:       req.body.url ? req.body.url : null,
-                image_url: req.body.image_url ? req.body.image_url : null,
-                rating:    req.body.rating ? req.body.rating : null
+    db.findAndModify('organizations',
+    {
+        'query': {
+            'slug': req.param('organization')
+        },
+        'update': {
+            '$push': {
+                'restaurants': {
+                    '_id': db.makeObjectId(),
+                    'yelp': {
+                        'name'     : req.body.name ? req.body.name : null,
+                        'id'       : req.body.id ? req.body.id : null,
+                        'location' : req.body.location ? req.body.location : null,
+                        'is_closed': req.body.is_closed ? req.body.is_closed : null,
+                        'url'      : req.body.url ? req.body.url : null,
+                        'image_url': req.body.image_url ? req.body.image_url : null,
+                        'rating'   : req.body.rating ? req.body.rating : null
+                    }
+                }
             }
-        };
-
-    db.update('organizations', { slug: req.param('organization') }, { '$push': { 'restaurants': restaurant }}, function (err, results) {
+        },
+        new: true,
+        upsert: true
+    },
+    function (err, results) {
         if (err) {
             console.log('There was an error adding this restaurant to your organization.  Sorry mate.', err);
         } else {
             console.log('Organization updated.');
 
-            db.findOne('organizations', { slug: req.param('organization') }, function (err, results) {
-                res.render('results', {
-                    locations: results.restaurants
-                });
+            res.render('results', {
+                'locations': results.restaurants
             });
         }
     });
 });
 
 router.delete('/:organization', function (req, res) {
-    db.update('organizations', { slug: req.param('organization') },
+    db.findAndModify('organizations',
     {
-        "$pull": {
-            "restaurants": {
-                "_id": db.getObjectId(req.body.id)
+        'query': {
+            'slug': req.param('organization')
+        },
+        'update': {
+            "$pull": {
+                "restaurants": {
+                    "_id": db.getObjectId(req.body.id)
+                }
             }
-        }
-    }, function (err, results) {
+        },
+        new: true,
+        upsert: true
+    },
+    function (err, results) {
         if (err) {
-            console.log('There was an error deleting the location, I think?', err);
+            console.log('There was an error deleting the location.', err);
         } else {
-            db.findOne('organizations', { slug: req.param('organization') }, function (err, results) {
-                res.render('results', {
-                    locations: results.restaurants
-                });
+            res.render('results', {
+                'locations': results.restaurants
             });
         }
     });
