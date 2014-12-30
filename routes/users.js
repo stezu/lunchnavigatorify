6a01f74511543e017b8fb552065e8fcb2db0ff51
Stephen Zuniga <stephen.zuniga001@gmail.com>
2014-12-30 01:17:22 -0500

    ghetto display users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../modules/database');
+
 /* GET users listing. */
 router.get('/', function(req, res) {
-    res.send('respond with a resource');
+	db.find('users',
+		{},
+		function (err, results) {
+			res.send(results);
+	});
 });
 
 module.exports = router;
